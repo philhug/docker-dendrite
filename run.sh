e2593dc37267340470a5253c369ad414906e38f1
Andreas Peters <ap@aventer.biz>
2017-12-20 15:29:36 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+cd /dendrite
+
+mkdir /etc/dendrite/
+
+openssl req -x509 -newkey rsa:4096 -keyout /etc/dendrite/server.key -out /etc/dendrite/server.crt -days 365 -nodes -subj /CN=$DOMAIN
+
+./bin/generate-keys -private-key /etc/dendrite/matrix_key.pem
+
+if [ "$FUNCTION" == "CLIENT_PROXY" ] 
+then
+  ./bin/client-api-proxy \
+  -bind-address ":8008" \
+  -client-api-server-url $CLIENT_URL \
+  -sync-api-server-url $SYNC_URL \
+  -media-api-server-url $MEDIA_URL \
+  -public-rooms-api-server-url $ROOM_URL 	
+fi
+
+if [ "$FUNCTION" == "CLIENT" ] 
+then
+  ./bin/dendrite-client-api-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "ROOM" ] 
+then
+  ./bin/dendrite-room-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "SYNC" ] 
+then
+  ./bin/dendrite-sync-api-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "MEDIA" ] 
+then
+  ./bin/dendrite-media-api-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "PUBLIC_ROOM" ] 
+then
+  ./bin/dendrite-public-rooms-api-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "FEDERATION_PROXY" ] 
+then
+  ./bin/federation-api-proxy --bind-address ":8448" --federation-api-url $FEDERATION_URL --media-api-server-url $MEDIA_URL 
+fi
+
+if [ "$FUNCTION" == "FEDERATION" ] 
+then
+  ./bin/dendrite-federation-api-server --config=/config/config.yaml
+fi
+
+if [ "$FUNCTION" == "FEDERATION_SENDER" ] 
+then
+  ./bin/dendrite-federation-sender-server --config=/config/config.yaml
+fi
+
+ps -ef
